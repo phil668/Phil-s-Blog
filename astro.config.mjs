07920154edb1bdea6d5163cc017983b7f70115bf
Phil Liu <1308972436@qq.com>
2024-12-04 22:41:21 +0800

    chore: add ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://moondust.cn",
   integrations: [mdx(), sitemap(), UnoCSS()],
 });
